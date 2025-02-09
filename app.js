d2a9bb7a8f15dfd82956327980ead1222de9ec10
Afshan kanwal <kanwalafshan2244@gmail.com>
2025-02-10 01:54:25 +0500

    assignment3

---
+++ app.js
@@ -0,0 +1,60 @@
+                    //  CHAPTER 9 
+// Qno:1 Write a program to take “city” name as input from user. If  user enters “Karachi”, welcome the user like this: 
+// “Welcome to city of lights”
+
+// let cityName = prompt("Enter Your City Name");
+// if (cityName=="karachi"){
+
+//         document.write("Welcome to the city of lights!")
+
+// }
+// else{
+    
+//         document.write("Welcome to" + " city")
+// }
+// Qno:2 . Write a program to take “gender” as input from user. If the user is male, give the message: Good Morning Sir. If the 
+// user is female, give the message: Good Morning Ma’am.
+// let gender1 = prompt("Enter Your Gender ")
+// let gender2 = prompt("Enter Your Gender ")
+// if(gender1=="male"){
+//     document.write("Good Morning Sir."+"<br />")
+// }
+// if(gender2=="female"){
+//     document.write("Good Morning Ma’am.")
+// }
+// else{
+//     document.write("Good Morning!")
+// }
+// Qno:3 Write a program to take input color of road traffic signal  from the user & show the message according to this table:
+// let signalColor1 = prompt("Enter the color of the traffic signal")
+// let signalColor2 = prompt("Enter the color of the traffic signal")
+// let signalColor3 = prompt("Enter the color of the traffic signal")
+// if(signalColor1=="red")
+// if(signalColor2=="yellow")
+// if(signalColor3=="green"){
+
+//     document.write("<table>")
+//     document.write("<tr>")
+//     document.write("<th>Signal color</th>")
+//     document.write("<th>Message</th>")
+//     document.write("</tr>")
+
+//     document.write("<tr>")
+//     document.write("<td>Red</td>")
+//     document.write("<td>Must Stop</td>")
+//     document.write("</tr>")
+
+//     document.write("<tr>")
+//     document.write("<td>Yellow</td>")
+//     document.write("<td>Ready to move</td>")
+//     document.write("</tr>")
+
+//     document.write("<tr>")
+//     document.write("<td>Green</td>")
+//     document.write("<td>Move Now</td>")
+//     document.write("</tr>")
+//     document.write("</table>")
+// }
+
+
+
